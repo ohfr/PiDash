2ca7c52e5730aaaf9c96de73e9ba3ef2cab55874
Daniel <danielmartin1205@gmail.com>
2023-07-30 00:53:36 -0400

    add restart and remove while loop

--- dash.py
+++ dash.py
@@ -20,62 +20,61 @@ class WorkerThread(QThread):
     def run(self):
         with can.Bus(channel='can0', bustype='socketcan') as bus:
             bus.set_filters([{ 'can_id': 0x3E0, 'can_mask': 0x3E1, 'extended': False }, { 'can_id': 0x360, 'can_mask': 0x360, 'extended': False }, { 'can_id': 0x370, 'can_mask': 0x371, 'extended': False }, { 'can_id': 0x368, 'can_mask': 0x368, 'extended': False }])
-            while True:
-                def convert(bytes: list, type):
-                    if bytes[0] == None and bytes[1] == None:
-                        return 0
-                    if bytes[0] == None:
-                        bytes[0] = 0
-                    if bytes[1] == None:
-                        bytes[1] = 0
-                    num = int.from_bytes(bytes, 'big')
-                    print(num)
-                    if type == 'kelvin':
-                        num *= .1
-                        return  (num - 273.15) * 9/5 + 32
-                    if type == 'lambda':
-                        num *= .001
-                        num *= 14.71
-                        return num
-                    if type == 'KPA':
-                        num*= .1
-                        if num <= 100:
-                            return -(num*.01)
-                        return num//6.895
-                    if type == 'KMH':
-                        num *= 10
-                        return num // 1.609
-            
-                def prettyPrint(msg):
-                    if msg.arbitration_id == COOLANT_AIR_TEMP:
-                        self.update_coolant.emit(convert(msg.data[:2], 'kelvin'))
-                        self.update_airTemp.emit(convert(msg.data[2:4], 'kelvin'))
-                        print('coolant: {}'.format(convert(msg.data[:2], 'kelvin')))
-                        print('air temp: {}'.format(convert(msg.data[2:4], 'kelvin')))
-                    elif msg.arbitration_id == RPM_MAP:
-                        self.update_rpm.emit(int.from_bytes(msg.data[:2], 'big'))
-                        self.update_boost.emit(convert(msg.data[2:4], 'KPA'))
-                        print('RPMS: {}'.format(int.from_bytes(msg.data[:2], 'big')))
-                        print('MAP (Boost): {}'.format(convert(msg.data[2:4], 'KPA')))
-                    elif msg.arbitration_id == WIDEBAND:
-                        self.update_airFuel.emit(convert(msg.data[:2], 'lambda'))
-                        print('AFR: {}'.format(convert(msg.data[:2], 'lambda')))
-                    elif msg.arbitration_id == SPEED:
-                        self.update_speed.emit(convert(msg.data[:2], 'KMH'))
-                        print('SPEED: {}'.format(convert(msg.data[:2], 'KMH')))
+            def convert(bytes: list, type):
+                if bytes[0] == None and bytes[1] == None:
+                    return 0
+                if bytes[0] == None:
+                    bytes[0] = 0
+                if bytes[1] == None:
+                    bytes[1] = 0
+                num = int.from_bytes(bytes, 'big')
+                print(num)
+                if type == 'kelvin':
+                    num *= .1
+                    return  (num - 273.15) * 9/5 + 32
+                if type == 'lambda':
+                    num *= .001
+                    num *= 14.71
+                    return num
+                if type == 'KPA':
+                    num*= .1
+                    if num <= 100:
+                        return -(num*.01)
+                    return num//6.895
+                if type == 'KMH':
+                    num *= 10
+                    return num // 1.609
+        
+            def prettyPrint(msg):
+                if msg.arbitration_id == COOLANT_AIR_TEMP:
+                    self.update_coolant.emit(convert(msg.data[:2], 'kelvin'))
+                    self.update_airTemp.emit(convert(msg.data[2:4], 'kelvin'))
+                    print('coolant: {}'.format(convert(msg.data[:2], 'kelvin')))
+                    print('air temp: {}'.format(convert(msg.data[2:4], 'kelvin')))
+                elif msg.arbitration_id == RPM_MAP:
+                    self.update_rpm.emit(int.from_bytes(msg.data[:2], 'big'))
+                    self.update_boost.emit(convert(msg.data[2:4], 'KPA'))
+                    print('RPMS: {}'.format(int.from_bytes(msg.data[:2], 'big')))
+                    print('MAP (Boost): {}'.format(convert(msg.data[2:4], 'KPA')))
+                elif msg.arbitration_id == WIDEBAND:
+                    self.update_airFuel.emit(convert(msg.data[:2], 'lambda'))
+                    print('AFR: {}'.format(convert(msg.data[:2], 'lambda')))
+                elif msg.arbitration_id == SPEED:
+                    self.update_speed.emit(convert(msg.data[:2], 'KMH'))
+                    print('SPEED: {}'.format(convert(msg.data[:2], 'KMH')))
 
-                # For testing purposes on a non CAN capable device
-                # def test():
-                #     self.update_coolant.emit(randrange(175, 190))
-                #     self.update_airTemp.emit(randrange(90, 120))
-                #     self.update_airFuel.emit(randrange(10, 22))
-                #     self.update_boost.emit(randrange(-18, 20))
-                #     self.update_rpm.emit(randrange(0, 9000))
-                #     self.update_speed.emit(randrange(0, 200))
+            # For testing purposes on a non CAN capable device
+            # def test():
+            #     self.update_coolant.emit(randrange(175, 190))
+            #     self.update_airTemp.emit(randrange(90, 120))
+            #     self.update_airFuel.emit(randrange(10, 22))
+            #     self.update_boost.emit(randrange(-18, 20))
+            #     self.update_rpm.emit(randrange(0, 9000))
+            #     self.update_speed.emit(randrange(0, 200))
 
-                # test()
-                notifier = can.Notifier(bus, [prettyPrint])
-                time.sleep(.5)
+            # test()
+            notifier = can.Notifier(bus, [prettyPrint])
+            time.sleep(.5)
 
 def main():
     # start the can bus
